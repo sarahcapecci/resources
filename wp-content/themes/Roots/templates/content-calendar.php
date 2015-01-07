2cd0350228a29451059aad33fa6718f6f3f36e70
Sarah <shlcapecci@gmail.com>
2015-01-07 17:11:09 -0500

    drafted Calendar page

---
+++ wp-content/themes/Roots/templates/content-calendar.php
@@ -0,0 +1,87 @@
+<!-- Wrapper -->
+<div>
+	<!-- LEFT side -->
+	<div>
+		<h2>Collective Calendar MONTH 1ST - LASTday</h2>
+		<!-- filter -->
+		<ul>
+		    <li>All /</li>
+		    <li>Show Only</li>
+		    <li>Meetings |</li>
+		    <li>Socials |</li>
+		    <li>Fundraisers</li>
+		</ul>
+		<!-- pagination -->
+		<span>Previous</span>
+		<h4>Weeks</h4>
+		<span>Next</span>
+		<button>Add to calendar</button>
+
+		<!-- Calendar -->
+		<div>
+			
+		</div>
+		<h2>Upcomming</h2>
+		<ul>
+			<!-- repeater for events -->
+		    <li>
+		    	<img src="" alt="" />
+		    	<h4>Event Name</h4>
+		    	<span>Month day, year</span>
+		    	<span>timeStart - timeEnd</span>
+		    	<span>Location, location</span>
+		    	<span>Specific Location</span>
+		    </li> 
+		</ul>
+		
+		<button><i class="fa fa-plus"></i>Add to Calendar</button>	
+	</div>
+	<!-- RIGHT side -->
+	<div>
+		<!-- latest event? -->
+		<div>
+			<h2>Event Name</h2>
+			<img src="" alt="" />
+			<p><img src="" alt="" /> Hosted by Organization Name</p>
+			<h4>Type of Event</h4>
+			<span>Weekday, Month day, YEAR</span>
+			<span>startTime - EndTime</span>
+			<span>Event Location</span>
+			<!-- social -->
+			<section>
+				<span>Eventbrite Registration Page</span>
+				<span>Facebook Event</span>
+				<button><i class="fa fa-share"></i>Share</button>
+			</section>
+
+			<section>
+				<h4>Notes</h4>
+				<p>Additional Details Previously Typed</p>
+			</section>
+		</div>
+		<!-- add an event form -->
+		<div>
+			<form>
+				<h2>Add Title: <input type="text" /></h2>
+				<label>Upload Image <input type="file"></label>
+				<p><img src="" alt="" /> Hosted by YOUR Organization Name</p>
+				<h4>Select type <input type="select"></h4>
+				<label>Date <input type="date"></label>
+				<!-- social -->
+				<section>
+					Eventbrite<input placeholder="Registration URL" type="url">
+					Facebook<input placeholder="Event Page" type="url">
+				</section>
+
+				<section>
+					<h4>Notes</h4>
+					<textarea placeholder="Add additional details here. They will be shown only for Members. Example: Special share instructions, discount codes, reminders to other organizations"></textarea>
+				</section>
+				<input type="submit" />
+			</form>
+		</div>
+		
+	</div>
+
+
+</div>

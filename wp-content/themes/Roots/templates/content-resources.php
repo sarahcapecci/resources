acbe3bf33eff334da47958c4c8d2c1e3081f0330
Sarah <shlcapecci@gmail.com>
2015-01-13 11:17:01 -0500

    search added in all resources

--- wp-content/themes/Roots/templates/content-resources.php
+++ wp-content/themes/Roots/templates/content-resources.php
@@ -1,22 +1,58 @@
 <!-- Left side -->
-<div class="left-side-top">
-	<h2>Resources</h2>
-	<ul class="select-filter">
-	    <li>All Resources /</li>
-	    <li class="middle-gray-txt">Show Only</li>
-	    <li class="blue-txt">Documents |</li>
-	    <li class="blue-txt">Links |</li>
-	    <li class="blue-txt active">Contacts</li>
-	</ul>
-	<p class="description"><strong>Welcome to the Torch Resources Database.</strong> This is your home to access a wealth of resources relevant specifically to youth-led organizations in Peel.</p>
+	<?php get_template_part('templates/resources', 'header') ?>
 	<!-- Search and Upload -->
 	<div class="search-file">
-		<label>Search <input class="text-input" type="text"><input type="submit" value=" "><i class="fa fa-search"></i></label>
+		<form action=" " method="POST">
+			<label>Search
+				<input class="text-input" type="text" name="userinput" />
+				<input type="submit" value=" "><i class="fa fa-search"></i>
+			</label>
+		</form>
 		<span><strong>or</strong></span>
 		<button>Upload a Resource <i class="fa fa-upload margin-left-5"></i></button>
 	</div>
 </div>
 <div class="left-side-bottom">
 	<h3>All Resources</h3>
+	<div>
+		<h3>Documents</h3>
+		<div>
+			<?php echo do_shortcode(
+				'[cfdb-html form="Upload Document" show="title,document-select,doc-description,doc-tags,file-upload,Submitted Login,Submitted" filelinks="url" stripbr="true" filter="doc-tags~~/.*$_POST(userinput).*/i||title~~/.*$_POST(userinput).*/i"]
+				<div class="document-card doc-type all ${document-select}">
+					<h4>${title}</h4>
+					<span class="type">${document-select}</span>
+					<img src="../wp-content/themes/Roots/assets/img/document.png" alt="">
+					<span class="doc-date">${Submitted}</span>
+					<div class="overlay">
+						<p class="font-light">${doc-description}</p>
+						<a class="orange-link" href="${file-upload}">Download File</a>
+					</div>
+				</div>
+				[/cfdb-html]'); ?>
+		</div>
+		<h3>Links</h3>
+		<div>
+			<?php echo do_shortcode('[cfdb-html form="Upload link" show="link-title,link-notes,link-tags,select-category,link-url" filelinks="url"  stripbr="true" filter="link-title~~/.*$_POST(userinput).*/i||link-tags~~/.*$_POST(userinput).*/i"]<div class="link-card">
+				<a href="${link-url}" target="_blank"><h4>${link-title}</h4></a>
+				<span><strong>Tags |</strong> ${link-tags}</span>
+				<span><strong>Notes |</strong> ${link-notes}</span>
+		</div>[/cfdb-html]'); ?>
+			
+		</div>
+		<h3>Contacts</h3>
+		<div>
+			<?php echo do_shortcode('[cfdb-html form="Upload Contact" show="contact-name,contact-organization,job-title,email,phone-number,support-info" stripbr="true" filter="contact-name~~/.*$_POST(userinput).*/i||support-info~~/.*$_POST(userinput).*/i"]<div class="contact-card">
+				<h4>${contact-name}</h4>
+					<span class="font-lg font-light">${contact-organization}</span>
+					<span class="info-divider"></span>
+					<p>${job-title}</p>
+					<a class="block" href="mailto:${email}">${email}</a>
+					<p>${phone-number}</p>
+					<h5 class="margin-top-20">SUPPORT</h5>
+					<span>${support-info}</span>
+			</div>[/cfdb-html]'); ?>
+		</div>
+	</div>
 </div>
 	
--- wp-content/themes/Roots/templates/resources-header.php
+++ wp-content/themes/Roots/templates/resources-header.php
@@ -1,10 +1,10 @@
 <div class="left-side-top">
 	<h2>Resources</h2>
 	<ul class="select-filter">
-	    <li>All Resources /</li>
+	    <li><a class="black-link-b" href="<?php echo esc_url(home_url('/')); ?>resources/">All Resources</a> /</li>
 	    <li class="middle-gray-txt">Show Only</li>
-	    <li class="blue-txt"><a href="<?php echo esc_url(home_url('/')); ?>resources/documents">Documents |</a></li>
-	    <li class="blue-txt"><a href="<?php echo esc_url(home_url('/')); ?>resources/links">Links |</a></li>
-	    <li class="blue-txt active"><a href="<?php echo esc_url(home_url('/')); ?>resources/contacts">Contacts</a></li>
+	    <li><a class="blue-link-b" href="<?php echo esc_url(home_url('/')); ?>resources/documents">Documents |</a></li>
+	    <li><a class="blue-link-b" href="<?php echo esc_url(home_url('/')); ?>resources/links">Links |</a></li>
+	    <li><a class="blue-link-b" href="<?php echo esc_url(home_url('/')); ?>resources/contacts">Contacts</a></li>
 	</ul>
 	<p class="description"><strong>Welcome to the Torch Resources Database.</strong> This is your home to access a wealth of resources relevant specifically to youth-led organizations in Peel.</p>

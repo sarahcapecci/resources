513476da3012e89b1c03e3579ef239074ba4d72b
Sarah <shlcapecci@gmail.com>
2015-01-16 15:33:23 -0500

    added event-add modal

--- wp-content/themes/Roots/templates/content-events.php
+++ wp-content/themes/Roots/templates/content-events.php
@@ -1,3 +1,7 @@
+<?php global $current_user;
+      get_currentuserinfo();
+?>
+
 <!-- LEFT side -->
 <div class="left-side events">
 	<h2>Collective Calendar MONTH 1ST - LAStday</h2>
@@ -8,7 +12,6 @@
 	    <li><a class="blue-link-b" href="<?php echo esc_url(home_url('/')); ?>requests/">Socials |</a></li>
 	    <li><a class="blue-link-b" href="<?php echo esc_url(home_url('/')); ?>resources/">Fundraisers</a></li>
 	</ul>
-	<button id="new-request"><i class="fa fa-plus margin-right-5"></i>Post Request</button>
 	<!-- Assets showing according to filter -->
 	<div class="request-modal all-modal">
 		<h4>Post a request to the bulletin for all RYR members to see and reply to.</h4>
@@ -235,7 +238,7 @@
 		</ul>
 	</div> -->
 	<!-- SELECTED Event -->
-	<div class="selected">
+	<div class="selected right-sidebar">
 		<h2>RYR Executive Meeting</h2>
 		<img class="event" src="<?php echo get_template_directory_uri(); ?>/assets/img/default-calendar.jpg" alt="" />
 		<?php echo get_avatar(get_the_author_meta( 'ID' ), 32); ?> <span class="margin-left-5">Hosted by Regional Youth Roundtable</span>
@@ -252,24 +255,26 @@
 		<p>Cupcake fruitcake bonbon unerdwear.com apple pie candy canes danish lollipop. Pastry muffin liquorice dessert.</p>
 	</div>
 	<!-- add an event form -->
-	<!-- <div>
+	<div class="new-event right-sidebar">
 		<form>
 			<h2>Add Title: <input type="text" /></h2>
 			<label>Upload Image <input type="file"></label>
-			<p><img src="" alt="" /> Hosted by YOUR Organization Name</p>
-			<h4>Select type <input type="select"></h4>
+			<label><?php echo get_avatar(get_the_author_meta( 'ID' ), 32); ?> Hosted by <input type="text" value="<?php echo $current_user->user_login; ?>"></label>
+			<select class="input-opac" name="carlist" form="carform">
+				<option value="meeting">Select Type</option>
+			  	<option value="meeting">Meeting</option>
+			  	<option value="socials">Socials</option>
+			  	<option value="fundraising">Fundraising</option>
+			</select>
 			<label>Date <input type="date"></label>
-			
-			<section>
-				Eventbrite<input placeholder="Registration URL" type="url">
-				Facebook<input placeholder="Event Page" type="url">
-			</section>
-
-			<section>
-				<h4>Notes</h4>
-				<textarea placeholder="Add additional details here. They will be shown only for Members. Example: Special share instructions, discount codes, reminders to other organizations"></textarea>
-			</section>
+			<label for="">Eventbrite<input class="input-opac" placeholder="Registration URL" type="url"></label>
+			<label for="">Facebook<input class="input-opac" placeholder="Event Page" type="url"></label>
 			<input type="submit" />
+			<section>
+				<h4 class="text-al-center">Notes</h4>
+				<textarea input-opac placeholder="Add additional details here. They will be shown only for Members. Example: Special share instructions, discount codes, reminders to other organizations"></textarea>
+			</section>
+			
 		</form>
-	</div> -->
+	</div>
 </div>

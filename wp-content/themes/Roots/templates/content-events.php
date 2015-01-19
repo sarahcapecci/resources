76fa884d6ee2de664522aafcfdde003f0a2c9c4d
Sarah <shlcapecci@gmail.com>
2015-01-19 14:14:25 -0500

    php calendar added, html deleted

--- wp-content/themes/Roots/templates/content-events.php
+++ wp-content/themes/Roots/templates/content-events.php
@@ -1,10 +1,108 @@
-<?php global $current_user;
-      get_currentuserinfo();
+<?php 
+
+global $current_user;
+get_currentuserinfo();
+
+/* draws a calendar */
+function draw_calendar($month,$year){
+
+	/* draw table */
+	$calendar = '<table cellpadding="0" cellspacing="0" class="calendar">';
+
+	/* table headings */
+	$headings = array('Sun','Mon','Tue','Wed','Thu','Fri','Sat');
+	$calendar.= '<tr class="calendar-row"><td class="calendar-day-head">'.implode('</td><td class="calendar-day-head">',$headings).'</td></tr>';
+
+	/* days and weeks vars now ... */
+	$running_day = date('w',mktime(0,0,0,$month,1,$year));
+	$days_in_month = date('t',mktime(0,0,0,$month,1,$year));
+	$days_in_this_week = 1;
+	$day_counter = 0;
+	$dates_array = array();
+
+	/* row for week one */
+	$calendar.= '<tr class="calendar-row-day">';
+
+	/* print "blank" days until the first of the current week */
+	for($x = 0; $x < $running_day; $x++):
+		$calendar.= '<td class="calendar-day-np"> </td>';
+		$days_in_this_week++;
+	endfor;
+
+	/* keep going with days.... */
+	for($list_day = 1; $list_day <= $days_in_month; $list_day++):
+		$calendar.= '<td class="calendar-day">';
+			/* add in the day number */
+			$calendar.= '<div class="day-number">'.$list_day.'</div>';
+
+			/** QUERY THE DATABASE FOR AN ENTRY FOR THIS DAY !!  IF MATCHES FOUND, PRINT THEM !! **/
+			$calendar.= str_repeat('<p> </p>',2);
+			
+		$calendar.= '</td>';
+		if($running_day == 6):
+			$calendar.= '</tr>';
+			if(($day_counter+1) != $days_in_month):
+				$calendar.= '<tr class="calendar-row-day">';
+			endif;
+			$running_day = -1;
+			$days_in_this_week = 0;
+		endif;
+		$days_in_this_week++; $running_day++; $day_counter++;
+	endfor;
+
+	/* finish the rest of the days in the week */
+	if($days_in_this_week < 8):
+		for($x = 1; $x <= (8 - $days_in_this_week); $x++):
+			$calendar.= '<td class="calendar-day-np"> </td>';
+		endfor;
+	endif;
+
+	/* final row */
+	$calendar.= '</tr>';
+
+	/* end the table */
+	$calendar.= '</table>';
+	
+	/* all done, return result */
+	return $calendar;
+}
+
+// /* sample usages */
+// echo draw_calendar(1,2015);
+
+/* date settings */
+$month = (int) ($_GET['month'] ? $_GET['month'] : date('m'));
+$year = (int)  ($_GET['year'] ? $_GET['year'] : date('Y'));
+
+/* select month control */
+$select_month_control = '<select name="month" id="month">';
+for($x = 1; $x <= 12; $x++) {
+	$select_month_control.= '<option value="'.$x.'"'.($x != $month ? '' : ' selected="selected"').'>'.date('F',mktime(0,0,0,$x,1,$year)).'</option>';
+}
+$select_month_control.= '</select>';
+
+/* select year control */
+$year_range = 7;
+$select_year_control = '<select name="year" id="year">';
+for($x = ($year-floor($year_range/2)); $x <= ($year+floor($year_range/2)); $x++) {
+	$select_year_control.= '<option value="'.$x.'"'.($x != $year ? '' : ' selected="selected"').'>'.$x.'</option>';
+}
+$select_year_control.= '</select>';
+
+/* "next month" control */
+$next_month_link = '<a href="?month='.($month != 12 ? $month + 1 : 1).'&year='.($month != 12 ? $year : $year + 1).'" class="control"><span><i class="fa fa-angle-double-right"></i></span></a>';
+
+/* "previous month" control */
+$previous_month_link = '<a href="?month='.($month != 1 ? $month - 1 : 12).'&year='.($month != 1 ? $year : $year - 1).'" class="control"><i class="fa fa-angle-double-left"></i></a>';
+
+/* bringing the controls together */
+$controls = '<form method="get">' .$previous_month_link.'  Month   '.$next_month_link.' </form>';
+
 ?>
 
 <!-- LEFT side -->
 <div class="left-side events">
-	<h2>Collective Calendar MONTH 1ST - LAStday</h2>
+	<h2>Collective Calendar </h2>
 	<ul class="select-filter">
 	    <li><a class="black-link-b" href="<?php echo esc_url(home_url('/')); ?>">All Events</a> /</li>
 	    <li class="middle-gray-txt">Show Only</li>
@@ -21,174 +119,15 @@
 	
 	<!-- pagination -->
 	<div class="calendar-pagination">
-		<span><i class="fa fa-angle-double-left"></i></span>
+		<?php echo $controls; ?>
+		<!-- <span><i class="fa fa-angle-double-left"></i></span>
 		<h5 class="inline-block">Weeks</h5>
-		<span><i class="fa fa-angle-double-right"></i></span>
+		<span><i class="fa fa-angle-double-right"></i></span> -->
 	</div>
-
 	<!-- Calendar -->
 	<!-- event list is a repeater that displays events in that certain date -->
 	<div id="calendar">
-		<table>
-		   <tr class="inactive">
-		   		<th><h5>Sun</h5></th>
-		      	<td class="day">
-					<span class="date">1</span>
-		      	</td>
-		      	<td class="day">
-		      		<span class="date">8</span>
-		      	</td>
-		   </tr>
-		   <tr>
-		   		<th><h5>Mon</h5></th>
-		      	<td class="day">
-		      		<span class="date">2</span>
-		      		<div class="event-list">
-		      			<span class="image"></span>
-		      			<h6>Event Name Here</h6>
-		      		</div>
-		      	</td>
-		      	<td class="day">
-		      		<span class="date">9</span>
-		      		<div class="event-list">
-		      			<span class="image"></span>
-		      			<h6>Event Name Here</h6>
-		      		</div>
-		      		<div class="event-list">
-		      			<span class="image"></span>
-		      			<h6>Event Name Here</h6>
-		      		</div>
-		      		<div class="event-list">
-		      			<span class="image"></span>
-		      			<h6>Event Name Here</h6>
-		      		</div>
-		      	</td>
-		   </tr>
-		   <tr>
-		   		<th><h5>Tue</h5></th>
-		      	<td class="day">
-		      		<span class="date">3</span>
-		      		<div class="event-list">
-		      			<span class="image"></span>
-		      			<h6>Event Name Here</h6>
-		      		</div>
-		      		<div class="event-list">
-		      			<span class="image"></span>
-		      			<h6>Event Name Here</h6>
-		      		</div>
-		      		<div class="event-list">
-		      			<span class="image"></span>
-		      			<h6>Event Name Here</h6>
-		      		</div>
-		      	</td>
-		      	<td class="day">
-		      		<span class="date">10</span>
-		      		<div class="event-list">
-		      			<span class="image"></span>
-		      			<h6>Event Name Here</h6>
-		      		</div>
-		      	</td>
-		   </tr>
-		   <tr>
-		   		<th><h5>Wed</h5></th>
-		      	<td class="day">
-		      		<span class="date">4</span>
-		      		<div class="event-list">
-		      			<span class="image"></span>
-		      			<h6>Event Name Here</h6>
-		      		</div>
-		      	</td>
-		      	<td class="day">
-		      		<span class="date">11</span>
-		      		<div class="event-list">
-		      			<span class="image"></span>
-		      			<h6>Event Name Here</h6>
-		      		</div>
-		      		<div class="event-list">
-		      			<span class="image"></span>
-		      			<h6>Event Name Here</h6>
-		      		</div>
-		      	</td>
-		   </tr>
-		   <tr>
-		   		<th><h5>Thu</h5></th>
-		      	<td class="day">
-		      		<span class="date">5</span>
-		      		<div class="event-list">
-		      			<span class="image"></span>
-		      			<h6>Event Name Here</h6>
-		      		</div>
-		      	</td>
-		      	<td class="day">
-		      		<span class="date">12</span>
-		      		<div class="event-list">
-		      			<span class="image"></span>
-		      			<h6>Event Name Here</h6>
-		      		</div>
-		      		<div class="event-list">
-		      			<span class="image"></span>
-		      			<h6>Event Name Here</h6>
-		      		</div><div class="event-list">
-		      			<span class="image"></span>
-		      			<h6>Event Name Here</h6>
-		      		</div>
-		      		<div class="event-list">
-		      			<span class="image"></span>
-		      			<h6>Event Name Here</h6>
-		      		</div>
-		      	</td>
-		   </tr>
-		   <tr>
-		   		<th><h5>Fri</h5></th>
-		      	<td class="day">
-		      		<span class="date">6</span>
-		      		<div class="event-list">
-		      			<span class="image"></span>
-		      			<h6>Event Name Here</h6>
-		      		</div>
-		      		<div class="event-list">
-		      			<span class="image"></span>
-		      			<h6>Event Name Here</h6>
-		      		</div>
-		      		<div class="event-list">
-		      			<span class="image"></span>
-		      			<h6>Event Name Here</h6>
-		      		</div>
-		      	</td>
-		      	<td class="day">
-		      		<span class="date">13</span>
-		      		<div class="event-list">
-		      			<span class="image"></span>
-		      			<h6>Event Name Here</h6>
-		      		</div>
-		      	</td>
-		   </tr>
-		   <tr class="inactive sat">
-		   		<th><h5>Sat</h5></th>
-		      	<td class="day">
-		      		<span class="date">7</span>
-		      	</td>
-		      	<td class="day">
-		      		<span class="date">14</span>
-		      	</td>
-		   </tr>
-		</table>
-	</div>
-	<h2 class="margin-top-20 margin-bottom-10">Upcomming</h2>
-	<!-- repeater for events -->
-	<div class="upcoming">
-	    <ul>
-	    	<li class="image"></li>
-	    	<li><h4>High School Health Conference</h4></li>
-	    	<li>
-	    		<p>November 1, 2014</p>
-	    		<p>3:30 - 5:30 PM</p>
-	    	</li>
-	    	<li>
-	    		<p>Mississauga Central Library</p>
-	    		<p>Meeting Room B</p>
-	    	</li>
-	    </ul>
+		<?php echo draw_calendar($month,$year); ?>
 	</div>
 
 	<div class="upcoming">
@@ -206,6 +145,21 @@
 	    </ul>
 	</div>
 
+
+	<div class="upcoming">
+	    <ul>
+	    	<li class="image"></li>
+	    	<li><h4>High School Health Conference</h4></li>
+	    	<li>
+	    		<p>November 1, 2014</p>
+	    		<p>3:30 - 5:30 PM</p>
+	    	</li>
+	    	<li>
+	    		<p>Mississauga Central Library</p>
+	    		<p>Meeting Room B</p>
+	    	</li>
+	    </ul>
+	</div>
 
 	<div class="upcoming">
 	    <ul>
